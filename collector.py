b36e2c3baadd6ec24b7260d0268759e712878b56
HN67 <hn67allardkids@gmail.com>
2019-02-18 09:32:14 -0700

    Stripped logic

--- collector.py
+++ collector.py
@@ -2,38 +2,40 @@
 
 # Import modules
 import tkinter as Tk
+from dataclasses import dataclass
+
+# Resources data class
+# Can be attached to tile, game, etc
+@dataclass
+class Resource:
+
+    name: str # Maybe create an enum of some sort instead of strings?
+    amount: int # Int makes sense, though maybe float?
+
+# Should a list of resources be a) a list, b) a set <- most likely, or c) a custom class?
+# One Issue with set is the neccesity to check for keys all the time
+# A class could creat unneccesary rigor if not done properly
 
 # Tile data holder class
 class Tile:
     
-    def __init__(self, resource: str, stage: int = 0):
+    def __init__(self, resources: set = None):
         
-        self.resource = resource
-        self.stage = stage
+        self.resources = resources
 
 # Abstract overhead game data class
 class Game:
 
     # Constructor
-    def __init__(self, size: int,
-                 maxTileLevel: int = 3,
-                 upgradeCosts: list = [{"metal":5},
-                                       {"metal":10},
-                                       {"metal":20}],
-                 startingResources: dict = {"metal": 0,
-                                            "crystal": 0,
-                                            "fuel": 0}
-                ):
+    def __init__(self, size: int):
 
         # Create tiles
         self.tiles = {}
 
         for x in range(size):
             for y in range(size):
-                self.tiles[(x,y)] = Tile("metal")
+                self.tiles[(x,y)] = Tile()
 
-        # Define resource stockpile stuff
-        self.resources = startingResources
 
 # Game controller class
 class Controller:
@@ -47,11 +49,6 @@ class Controller:
     def tick(self):
         pass
 
-    def upgrade_tile(self, x, y):
-        pass
-
-    def downgrade_tile(self, x, y):
-        pass
 
 # Main app class
 class Main:
