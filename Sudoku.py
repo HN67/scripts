fbcfc48ba08dd65e7ab618a29c3eb5cdad3df99e
HN67 <hn67allardkids@gmail.com>
2019-04-07 20:03:00 -0600

    Finished add solve method

--- Sudoku.py
+++ Sudoku.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 @dataclass
 class Tile:
     """Stores the value and box position of a tile"""
-    
+
     value: int
     row: int
     column: int
@@ -99,15 +99,122 @@ class Sudoku:
         # Subtract the sets to find what is valid
         return possible - invalid
 
-def main():
+    def empty_tiles(self):
+        """Returns all tiles that are empty; in a list with no specific order"""
+        return [tile for tile in self.board.values() if tile.value == 0]
 
+    def copy(self):
+        """Returns a copy of this Sudoku board"""
+
+        # Create the new, empty board
+        new = Sudoku()
+
+        # Copy the value of each tile over
+        for position, tile in self.board.items():
+            new.set_value(*position, tile.value)
+
+        # Return the copy
+        return new
+
+    def solve(self):
+        """Attempts to solve this sudoku
+        Returns True if sucsesfull, and mutates the board to solved
+        Returns False if unsucsesfull, and leaves the board in original state
+        """
+
+        # Find all the empty (modifiable) tiles
+        spaces = self.empty_tiles()
+
+        # cursor is a pointer to the current space being manipulated
+        cursor = 0
+
+        # Solve the sudoku until every space has been solved
+        while cursor < len(spaces) or cursor == -1:
+
+            # Increase the value at the cursor
+            value = spaces[cursor].value + 1
+
+            # If the value is valid, set it and move on to the next tile
+            if value in self.valid_choices(spaces[cursor].row, spaces[cursor].column):
+                spaces[cursor].value = value
+                cursor += 1
+            # If invalid, still set it for now, will be incremented next turn
+            else:
+                spaces[cursor].value = value
+
+            # Check if the value has overflowed (there were no possible values)
+            if value > self.size:
+                # Reset (clear) this tile
+                spaces[cursor].value = 0
+                # Move back
+                cursor -= 1
+
+            #print(cursor, value)
+
+        # Check if solving was unsucsessful
+        if cursor == -1:
+            return False
+        else:
+            return True
+
+    def __str__(self):
+        """Returns a string representation of the board state"""
+
+        # Create a 2d array of lists
+        output = []
+
+        # Add the values into the 2d list
+        for row in range(self.size):
+
+            # Add horizontal seperator every boxHeight
+            if row % self.boxHeight == 0 and row != 0:
+                output.append("")
+
+            # Prepare current row
+            output.append([])
+
+            for column in range(self.size):
+
+                # Add vertical bar every boxWidth
+                if column % self.boxWidth == 0 and column != 0:
+                    output[-1].append(" ")
+
+                # Replace 0 with a _
+                if self.value(row, column) == 0:
+                    output[-1].append("-")
+                else:
+                    output[-1].append(str(self.value(row, column)))
+
+        # Join each row into a string
+        output = [" ".join(row) for row in output]
+
+        # Join all the rows with \n between them
+        return "\n".join(output)
+
+
+
+def main():
+    """Main function for testing"""
+
+    # Create test board
     sudo = Sudoku()
 
-    for tile in sudo.board:
-        print(tile, sudo.board[tile].box, sudo.board[tile].value)
+    # Populate test board
+    sudo.set_value(3, 3, 5)
+    sudo.set_value(5, 5, 6)
+    sudo.set_value(0, 8, 1)
+    sudo.set_value(8, 0, 7)
 
-    print(sudo.invalid_choices(3, 3))
-    print(sudo.valid_choices(3, 3))
+    # Show test board
+    print(sudo)
+
+    # Solve test board
+    sudo.solve()
+
+    # Seperate before/after
+    print()
+    # Show solved board
+    print(sudo)
 
 if __name__ == "__main__":
     main()
