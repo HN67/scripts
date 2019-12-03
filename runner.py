404e5946086bc049506d4b762421327759e57e43
HN67 <hn67allardkids@gmail.com>
2019-12-03 09:49:57 -0700

    Change block size and rate, add image to blocks

--- runner.py
+++ runner.py
@@ -1,8 +1,17 @@
 """Sidescrolling runner game"""
 
+# Import core modules
+import os
+import typing
 import random
 
+# Import pygame
 import pygame
+
+# Define path function that turns a relative path into an absolute path based on file location
+def path(local: str) -> str:
+    """Returns the absolute path of local path, based on this file location"""
+    return os.path.join(os.path.dirname(__file__), local)
 
 # Init pygame
 pygame.init()
@@ -10,19 +19,18 @@ pygame.init()
 class Block(pygame.sprite.Sprite):
     """General block that is run past"""
 
-    def __init__(self, rect: pygame.Rect):
+    def __init__(self, position: typing.Tuple[int, int], image: pygame.Surface):
 
         # Call super initator to be able to interact with pygame sprite tools
         super().__init__()
 
-        # Reference rect
-        self.rect = rect
+        # Reference image
+        self.image = image
 
-        # Create image
-        self.image = pygame.Surface(self.rect.size)
-
-        # Background fill the image with white for now
-        self.image.fill((255, 255, 255))
+        # Create rect
+        self.rect = self.image.get_rect()
+        self.rect.x = position[0]
+        self.rect.y = position[1]
 
     def update(self):
         """Moves the block by the given speed"""
@@ -35,21 +43,26 @@ class Block(pygame.sprite.Sprite):
 # Create clock
 clock = pygame.time.Clock()
 
+# Define viewbox
+viewbox = pygame.Rect(0, 0, 500, 500)
+
 # Setup window
 # TODO should create and use config values
-screen = pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode(viewbox.size)
 pygame.display.set_caption("Runner")
 tps = 60
+
+# Load images
+images = {
+    "block": pygame.image.load(path("block.png")).convert()
+}
 
 # Initiate block group
 blocks = pygame.sprite.Group()
 
-# The length of a full cycle of game logic in ticks (e.g. producing new blocks)
-cycle_length = 60
-
 # Main loop
 running = True
-tick = -1
+tick = 0
 while running:
 
     # Dump event queue into reference
@@ -65,20 +78,20 @@ while running:
     # Skips the rest of the loop if the program is quitting
     if running:
 
-        # Increment tick
-        tick = (tick + 1) % cycle_length
-
-        # Create new block every 10 ticks
-        if tick % 5 == 0:
-            blocks.add(Block(pygame.Rect(400, random.randint(0, 15)*25, 25, 25)))
+        # Create new block every other space based on width ticks
+        if tick % 20 == 0:
+            blocks.add(Block((500, random.randint(0, 9)*50), images["block"]))
 
         # Update blocks
         blocks.update()
 
-        screen.fill((0, 0, 0))
+        screen.fill((100, 100, 100))
         blocks.draw(screen)
 
         # Flip display
         pygame.display.flip()
+
+        # Increment tick
+        tick = (tick + 1)# % cycle_length
         # Limit to determined tps
         clock.tick(tps)
